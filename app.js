abe2cd173e6dd14c483056fc6837f2c2222ccfe7
svt <sv-work@yandex.ru>
2013-07-03 17:00:40 +0400

    add bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.configure(function() {
     app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
     app.set('view engine', 'html')                      
     app.set('views', __dirname + "/tpl");
+    app.set("view options", {layout: 'layout.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
     app.use(express.static(__dirname + "/assets"));     // Делаем файлы из папки public доступными на сайте
 });
 
