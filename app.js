e2c4867dc841bd1224f291bc7ac3c95b02dcfb47
svt <sv-work@yandex.ru>
2013-07-03 16:31:39 +0400

    add bootstrap

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+    app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
+    app.set('view engine', 'html')                      
+    app.set('views', __dirname + "/tpl");
+    app.use(express.static(__dirname + "/assets"));     // Делаем файлы из папки public доступными на сайте
+});
+
+app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
+    res.render('index.html');
+});
+
+
+var port = process.env.PORT || 5000;       
+app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
+console.log("Listening at " + port)        // Пишем в консоль, что запустились
